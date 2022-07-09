3e9faf011f9759c7989f38a3a9fca980372d639b
0gy <76845848+OgyDotMy@users.noreply.github.com>
2022-07-09 22:10:14 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include("core/db.php");
+?>
+
+<?php 
+include("parts/header.php");
+?>
+    <!-- content -->
+    <div class="content">
+        <div class="container">
+            <p>Welcome to Online Worker Form Submit</p>
+        </div>
+    </div>
+    <!-- end content -->
+
+<?php 
+include("parts/footer.php");
+?>
