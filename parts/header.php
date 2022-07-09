a258cd16e702161af1de8e8250e12905878c60a1
0gy <76845848+OgyDotMy@users.noreply.github.com>
2022-07-09 22:12:27 +0800

    Create header.php

---
+++ parts/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Pekerja</title>
+    <link rel="stylesheet" href="https://php-learning-whit3r0s31337431268.codeanyapp.com/form-pekerja/assets/css/style.css">
+</head>
+<body>
+    <!-- header -->
+    <div class="header">
+        <div class="container">
+            <div class="d-flex jc-between ai-center">
+                <div class="header-logo">
+                    <h1><a href="index.php">Form Pekerja Online</a></h1>
+                </div>
+                <div class="header-menu">
+                    <a href="index.php">Home</a>
+                    <a href="submitpekerja.php">Submit Pekerja</a>
+                    <a href="listpekerja.php">List Pekerja</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end header -->
